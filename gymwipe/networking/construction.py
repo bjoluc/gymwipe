4bc64e550d4e186a44dff77ff93cfe0fc77c4d2f
bjoluc <25661029+bjoluc@users.noreply.github.com>
2018-07-06 23:14:37 +0200

    extended construction module, added simpy support

--- gymwipe/networking/construction.py
+++ gymwipe/networking/construction.py
@@ -2,22 +2,29 @@
 Contains classes for building network stack representations.
 """
 from typing import Callable, Any
+from simpy.events import Event
+from gymwipe.simtools import SimMan
 
 class Gate:
-    """
+    """   
     Todo:
         * finish documentation
+
     """
 
     class Port:
+        """
+
+        """
 
         def __init__(self):
             self._onSendCallables = set()
+            self._sendEvent = None
 
         def _addDest(self, dest: Callable[[Any], None]) -> None:
             """
             Args:
-                dest: A callback function that a message will be sent to when send is called on the Port
+                dest: A callback function that will be called with a message object when send is called on the Port
             """
             self._onSendCallables.add(dest)
         
@@ -25,7 +32,7 @@ class Gate:
 
         def connectTo(self, port: 'Gate.Port') -> None:
             """
-            Connects this Port to the provided Port. Thus, if send is called on this Port, send will also be called on the provided Port.
+            Connects this `Port` to the provided port. Thus, if `send` is called on this port, it will also be called on the provided port.
 
             Args:
                 port: The port for the connection to be established to
@@ -35,20 +42,38 @@ class Gate:
         # sending messages
 
         def send(self, message: Any):
+            """
+            Sends the object provided as `message` to all connected ports and registered callback functions (if any).
+            """
+            self._triggerSendEvent(message)
             for send in self._onSendCallables:
                 send(message)
+        
+        # SimPy events
+
+        @property
+        def sendEvent(self) -> Event:
+            """simpy.events.Event: A SimPy `Event` that is triggered when `send` is called on this port."""
+            if self._sendEvent is None:
+                self._sendEvent = Event(SimMan.env)
+            return self._sendEvent
+
+        def _triggerSendEvent(self, message) -> None:
+            if self._sendEvent is not None:
+                self._sendEvent.succeed(message)
+                self._sendEvent = None
     
 
-    def __init__(self, inputCallback: Callable[[Any], None] = None):
+    def __init__(self, name: str, inputCallback: Callable[[Any], None] = None):
         """
         Args:
-            inputCallback: The callback that will be used when a message is sent to the input Port.
+            inputCallback: A callback function that will be used when a message is sent to the input Port.
         """
         
         self.input = self.Port()
         if callable(inputCallback):
             self.input._addDest(inputCallback)
-        self._output = self.Port()
+        self.output = self.Port()
 
     # Connecting Gates
 
@@ -59,7 +84,7 @@ class Gate:
         Args:
             port: The port to connect this Gate's output to
         """
-        self._output.connectTo(port)
+        self.output.connectTo(port)
     
     def connectInputTo(self, port: Port) -> None:
         """
@@ -72,39 +97,54 @@ class Gate:
     
     def biConnectWith(self, gate: 'Gate') -> None:
         """
-        Shorthand for self.connectOutputTo(gate.input); gate.connectOutputTo(self._output)
+        Shorthand for `self.connectOutputTo(gate.input); gate.connectOutputTo(self._output)`
 
         Args:
-            gate: The Gate for the bidirectional connection to be established to
+            gate: The `Gate` for the bidirectional connection to be established to
         """
         self.connectOutputTo(gate.input)
         gate.connectOutputTo(self.input)
-
+    
+    # SimPy events vor message handling
+    
+    @property
+    def receivesMessage(self):
+        """Event: A SimPy `Event` that is triggered when the input port receives a message"""
+        return self.input.sendEvent
+    
 class Module:
     """
     Attributes:
         gates(Dict[str, Gate]): The Module's outer Gates
-        _innerModules(Dict[str, Module]): The Module's nested Modules
+        subModules(Dict[str, Module]): The Module's nested Modules
     """
 
     def __init__(self, name: str):
-        """
-        Args:
-            name: The Module's name
-        """
         self._name = name
         self.gates = {}
-        self._subModules = {}
+        self.subModules = {}
     
-    def _addGate(self, name: str, gate: Gate) -> None:
+    def _addGate(self, name: str, gate: Gate = None) -> None:
+        """
+        Adds a new `Gate` to the *gates* dictionary, indexed by the name passed.
+        
+        Args:
+            name: The name for the gate to be accessed by
+            gate: The `Gate` object to be added. If not provided, a new `Gate` will be
+                instantiated using a concatenation of the Module's name property and *name* as its name.
+        """
         if name in self.gates:
             raise ValueError("A gate named '%s' already exists." % str)
+        if gate is None:
+            gate = Gate(self.name + '.' + name)
         self.gates[name] = gate
     
     def _addSubModule(self, name: str, module: 'Module') -> None:
-        if name in self._subModules:
+        if name in self.subModules:
             raise ValueError("A sub module named '%s' already exists." % str)
-        self._subModules[name] = module
+        self.subModules[name] = module
     
-    def getName(self) -> str:
+    @property
+    def name(self):
+        """str: The Module's name"""
         return self._name
--- gymwipe/networking/test/test_construction.py
+++ gymwipe/networking/test/test_construction.py
@@ -1,6 +1,7 @@
 import pytest
 from pytest_mock import mocker
 from gymwipe.networking.construction import Gate, Module
+from gymwipe.simtools import SimMan
 
 # Note: When mocking member functions of a class:
 # Disable pylint warnings due to dynamically added member functions (assert_called_with) by # pylint: disable=E1101
@@ -11,33 +12,33 @@ def test_ports(mocker):
     g2_receive = mocker.Mock()
 
     # Create two gates and connect them bidirectionally
-    g1 = Gate(g1_receive)
+    g1 = Gate("g1", g1_receive)
     assert g1.input._onSendCallables == {g1_receive}
 
-    g2 = Gate(g2_receive)
+    g2 = Gate("g2", g2_receive)
     assert g2.input._onSendCallables == {g2_receive}
 
     g1.connectOutputTo(g2.input)
-    assert g1._output._onSendCallables == {g2.input.send}
+    assert g1.output._onSendCallables == {g2.input.send}
 
     g2.connectOutputTo(g1.input)
-    assert g2._output._onSendCallables == {g1.input.send}
+    assert g2.output._onSendCallables == {g1.input.send}
 
     # Test message sending
     msg1 = 'test message 1'
     msg2 = 'test message 2'
 
-    g1._output.send(msg1)
+    g1.output.send(msg1)
     g2_receive.assert_called_with(msg1)
 
-    g2._output.send(msg2)
+    g2.output.send(msg2)
     g1_receive.assert_called_with(msg2)
 
-def test_modules(mocker):
+def test_module_functions():
     m = Module('test module')
-    assert m.getName() == 'test module'
+    assert m.name == 'test module'
 
-    g1, g2 = (Gate(), Gate())
+    g1, g2 = (Gate("g1"), Gate("g2"))
     m._addGate('gate 1', g1)
     m._addGate('gate 2', g2)
     assert m.gates['gate 1'] == g1
@@ -47,6 +48,70 @@ def test_modules(mocker):
     m1, m2 = (Module('module 1'), Module('module 2'))
     m._addSubModule('sub module 1', m1)
     m._addSubModule('sub module 2', m2)
-    assert m._subModules['sub module 1'] == m1
-    assert m._subModules['sub module 2'] == m2
-    assert m._subModules == {'sub module 1': m1, 'sub module 2': m2}
+    assert m.subModules['sub module 1'] == m1
+    assert m.subModules['sub module 2'] == m2
+    assert m.subModules == {'sub module 1': m1, 'sub module 2': m2}
+
+def test_module_simulation():
+    # Connect two modules in a cycle and let them pass around a message object in both directions
+    #
+    #      <----------------->
+    # |----a-----|      |----a-----|
+    # | module 1 |      | module 2 |
+    # |----b-----|      |----b-----|
+    #      <----------------->
+
+    class TestModule(Module):
+        def __init__(self, name):
+            super(TestModule, self).__init__(name)
+            self._addGate("a")
+            self._addGate("b")
+            self.msgReceivedCount = {"a": 0, "b": 0}
+            self.msgVal = None
+            SimMan.registerProcess(self.process("a", "b"))
+            SimMan.registerProcess(self.process("b", "a"))
+        
+        def process(self, fromGate: str, toGate: str):
+            while(True):
+                # Listen on gate fromGate and proxy messages
+                print("TestModule " + self.name + " port " + fromGate + " waiting for message")
+                msg = yield self.gates[fromGate].receivesMessage
+
+                print("TestModule " + self.name + " port " + fromGate + " received message " + str(msg))
+                self.msgVal = msg
+                self.msgReceivedCount[fromGate] += 1
+                msg += 1
+                yield SimMan.env.timeout(1) # wait 1 time step before sending
+
+                # change the direction every 10 times a message has been passed
+                if msg % 10 == 0:
+                    self.gates[fromGate].output.send(msg)
+                else:
+                    self.gates[toGate].output.send(msg)
+    
+    m1 = TestModule("1")
+    m2 = TestModule("2")
+
+    m1.gates["b"].biConnectWith(m2.gates["b"])
+    m2.gates["a"].biConnectWith(m1.gates["a"])
+
+    def simulation():
+        # send the test message (a zero)
+        print("sending message")
+        m1.gates["a"].input.send(1)
+
+        # wait 50 timesteps
+        yield SimMan.env.timeout(20)
+        assert m1.msgVal == 19
+        assert m2.msgVal == 20
+        yield SimMan.env.timeout(20)
+
+        # assertions
+        assert m1.msgReceivedCount["a"] == 10
+        assert m1.msgReceivedCount["b"] == 10
+        assert m2.msgReceivedCount["a"] == 10
+        assert m2.msgReceivedCount["b"] == 10
+    
+    SimMan.registerProcess(simulation())
+    SimMan.runSimulation(50)
+    
