56332c209c81042d5001771d32986b4079d157b1
bjoluc <25661029+bjoluc@users.noreply.github.com>
2018-07-14 12:02:33 +0200

    extended SimulationManager, added SimTimePrepender

--- gymwipe/simtools.py
+++ gymwipe/simtools.py
@@ -1,13 +1,17 @@
 """
-Package for simulation tools
+Module for simulation tools
 """
-from typing import Generator
+import logging
+from typing import Generator, Any
 from simpy import Environment
 from simpy.events import Event
 
+logger = logging.getLogger(__name__)
+
 class SimulationManager:
     """
-    The Simulation Manager offers methods and properties for controlling and accessing the SimPy simulation.
+    The Simulation Manager offers methods and properties for managing and accessing a SimPy simulation.
+    Note: Do not create instances on your own. Use the existing simtools.SimMan instance instead.
     """
     
     _env = None
@@ -21,7 +25,7 @@ class SimulationManager:
     def env(self):
         """simpy.Environment: The SimPy environment object belonging to the current simulation"""
         if self._env is None:
-            self.resetEnvironment()
+            self.initEnvironment()
         return self._env
 
     def registerProcess(self, process: Generator[Event, None, None]) -> None:
@@ -35,12 +39,49 @@ class SimulationManager:
 
     def runSimulation(self, timesteps: int) -> None:
         self.env.run(until=timesteps)
+        logger.info("SimulationManager: Starting simulation...")
     
-    def resetEnvironment(self) -> None:
+    def initEnvironment(self) -> None:
         """
         Destroys the existing SimPy environment (if there is one) and creates a new one.
         The next `runSimulation` call will start a new simulation.
         """
         self._env = Environment()
+        logger.debug("SimulationManager: Initialized environment")
+    
+    def timeout(self, steps: int, value: Any = None) -> Event:
+        """
+        Shorthand for env.timeout(steps, value)
+        """
+        return self.env.timeout(steps, value)
+    
+    def timeoutUntil(self, triggerTime: int, value: Any = None) -> Event:
+        """
+        Returns a SimPy event that is triggerd at the time step specified by `triggerTime`.
+
+        Args:
+            triggerTime: When to trigger the event
+        """
+        now = self.now
+        if triggerTime > now:
+            return self.timeout(triggerTime-now, value)
+        else:
+            return self.timeout(0, value)
 
 SimMan = SimulationManager()
+"""A globally accessible SimulationManager instance to be used whenever a SimPy simulation is envolved"""
+
+class SimTimePrepender(logging.LoggerAdapter):
+    """
+    A LoggerAdapter that prepends the current simulation time step
+    (fetched by requesting SimMan.now) to any log message sent via `logger`.
+    """
+    def __init__(self, logger: logging.Logger):
+        """
+        Args:
+            logger: The `Logger` to be wrapped by the SimTimePrepender LoggerAdapter
+        """
+        super(SimTimePrepender, self).__init__(logger, {})
+
+    def process(self, msg, kwargs):
+        return "[Time step: {}] {}".format(SimMan.now, msg), kwargs
