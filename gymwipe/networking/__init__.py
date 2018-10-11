cf4999797846e9de6243de09ac53657a26964a09
bjoluc <25661029+bjoluc@users.noreply.github.com>
2018-10-11 12:46:43 +0200

    updated networking package decription

--- gymwipe/networking/__init__.py
+++ gymwipe/networking/__init__.py
@@ -1,3 +1,5 @@
 """
-The networking package provides the NetworkDevice baseclass along with network stack models, a framework for network stack modeling and physical channel simulations.
+The networking package provides a framework for network stack modeling and
+physical channel simulations, as well as network stack implementations and
+:class:`gymwipe.devices.Device` implementations that run network stacks.
 """
