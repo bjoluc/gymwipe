1b7452ef2b9904e054d0028eede43e1faccbbce9
bjoluc <25661029+bjoluc@users.noreply.github.com>
2018-12-06 23:01:06 +0100

    locked dependencies

--- setup.py
+++ setup.py
@@ -8,11 +8,14 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='gymwipe',
-      version='1.0',
-      description='Gym Wireless Plant Environment',
-      author='bjoluc',
-      url='https://github.com/bjoluc/gymwipe',
-      packages=find_packages(),
-      long_description=long_description,
-      long_description_content_type='text/markdown'
-     )
+    version='1.0',
+    description='Gym Wireless Plant Environment',
+    author='bjoluc',
+    url='https://github.com/bjoluc/gymwipe',
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    extras_require={
+    'ode': ['py3ode','pygame'],
+    },
+)
