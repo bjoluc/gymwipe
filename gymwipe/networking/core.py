6b250f0d7b662ad033c3130c1ac948faf6cae08c
bjoluc <25661029+bjoluc@users.noreply.github.com>
2018-12-08 18:49:23 +0100

    deleted old core module

--- gymwipe/networking/core.py
+++
@@ -1,9 +0,0 @@
-"""
-Core components for networking simulation
-"""
-from gymwipe.devices import Device
-
-class NetworkDevice(Device):
-    """
-    Deprecated, please use :class:`gymwipe.devices.Device` instead.
-    """
