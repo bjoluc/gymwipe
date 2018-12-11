8582f8a14c24cedb8b1412d7d0d23694732bcd3d
bjoluc <25661029+bjoluc@users.noreply.github.com>
2018-12-11 20:44:22 +0100

    fixed class reference

--- gymwipe/networking/__init__.py
+++ gymwipe/networking/__init__.py
@@ -1,6 +1,6 @@
 """
-The networking package provides a framework for network stack modeling and
+The networking package provides classes for network stack modeling and
 wireless data transmission simulations, as well as network stack implementations
-and :class:`~gymwipe.devices.Device` implementations that run network stacks.
+and :class:`~gymwipe.devices.core.Device` implementations that run network stacks.
 """
 from gymwipe.networking.devices import NetworkDevice
